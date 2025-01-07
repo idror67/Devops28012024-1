d307fa8459329ecca5976fb235dca0402a660eca
idror <144365018+idror67@users.noreply.github.com>
2025-01-07 22:12:47 +0200

    Update password comment in commands.sh for clarity

--- Lesson77Ansible3/commands.sh
+++ Lesson77Ansible3/commands.sh
@@ -7,7 +7,7 @@ ssh-keygen -t ed25519 -C "default"
 # copy ssh key to remote server
 ssh-copy-id -i ~/.ssh/id_ed25519.pub root@<your ip>
 # username root
-# password <your password> -> from linode
+# password <your password>git pull https://github.com/fullstackjava082023/Devops28012024.git -> from linode
 
 # establish ssh connection
 ssh root@172.232.209.99
